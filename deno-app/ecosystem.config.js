9753256fd1c7920dd3c87d5cd85b5714df8729d7
andrew1931 <andrewk1931@gmail.com>
2025-02-16 12:07:32 +0100

    feat: move ecosystem config

---
+++ deno-app/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "pdf-swiper",
+            script: "./src/main.ts",
+            interpreter: "deno",
+            interpreterArgs: "run --env-file=../.env --allow-env --allow-net --allow-read --allow-write --allow-ffi --allow-sys",
+        },
+    ],
+};
