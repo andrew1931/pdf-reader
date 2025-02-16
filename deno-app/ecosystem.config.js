93b835afafd997ac9f3a8a359fcc2cb90a80f182
andrew1931 <andrewk1931@gmail.com>
2025-02-16 12:42:09 +0100

    revert: remove cwd from ecosystem.config

--- deno-app/ecosystem.config.js
+++ deno-app/ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps: [
         {
             name: "pdf-swiper",
-            cwd: "/var/www/pdf-swiper/deno-app",
             script: "./src/main.ts",
             interpreter: "deno",
             interpreterArgs: "run --env-file=.env --allow-env --allow-net --allow-read --allow-write --allow-ffi --allow-sys",
