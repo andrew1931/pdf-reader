c6866f9aa47899e113bc4bfa4fe3f821e5224eb5
andrew1931 <andrewk1931@gmail.com>
2025-02-16 12:27:49 +0100

    feat: add cwd to ecosystem.config

--- deno-app/ecosystem.config.js
+++ deno-app/ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps: [
         {
             name: "pdf-swiper",
+            cwd: "/var/www/pdf-swiper/deno-app",
             script: "./src/main.ts",
             interpreter: "deno",
             interpreterArgs: "run --env-file=.env --allow-env --allow-net --allow-read --allow-write --allow-ffi --allow-sys",
