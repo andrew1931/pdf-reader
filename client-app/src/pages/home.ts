ae0a4c186c45367402b9e81fb638517b49e7dfa2
andrew1931 <andrewk1931@gmail.com>
2025-02-23 11:16:04 +0100

    feat: edit subtitle text

--- client-app/src/pages/home.ts
+++ client-app/src/pages/home.ts
@@ -73,7 +73,7 @@ export const HomePage = (): HTMLElement => {
     return Layout(
         ContentContainer(
             H1( "Welcome to \n PDF swiper"),
-            P("Choose a document from your device using the button below to swipe it like a book"),
+            P("Choose a document from your device using \n the button below to swipe it like a book"),
             EssentialLinks(),
             LocalFileButton(),
             DocumentsTitle(orderBy.target),
